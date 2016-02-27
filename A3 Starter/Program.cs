71588a5cb5acc8c0ee107851bac16f5c95c595df
Mihir Doshi <mihir7293@gmail.com>
2016-02-27 15:47:34 +0530

    Relocation of files

---
+++ A3 Starter/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Project : A3 starter
+ * File : Program.cs
+ * Description : This file contains code to start A3Client.exe.
+ *               This project is made by taking help from http://forum.ragezone.com/f98/
+ */
+using System;
+using System.Diagnostics;
+
+namespace A3_Starter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Creates process
+                Process a3Start = new Process();
+                // Assigns the exe name to start
+                a3Start.StartInfo.FileName = "A3Client.exe";
+                // Parameter needed to start A3Client.exe
+                a3Start.StartInfo.Arguments = "3216473353";
+                // Starts exe mentioned above
+                a3Start.Start();
+                // Exits this application.
+                Environment.Exit(0);
+            }
+            catch
+            {
+                // Exits this application.
+                Environment.Exit(0);
+            }
+        }
+    }
+}
